a457e30d353ef63d67b74dee9e8ef341b5a0aa5d
Khairul Aqram <khairulaqram21@gmail.com>
2023-12-24 23:23:37 +0700

    fix-bug

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>SiKecilSehat - {{ $title }}</title>
+    <title>SiKecilSehat - {{ $title ?? 'Error' }}</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
