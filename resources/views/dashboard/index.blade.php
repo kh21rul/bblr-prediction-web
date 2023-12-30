9ac0a81cb0191fbe36c721ce67cc557a4de686ea
Khairul Aqram <khairulaqram21@gmail.com>
2023-12-30 14:50:55 +0700

    perbaiki tampilan dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -10,8 +10,8 @@
             <div class="row">
                 <div class="col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
-                        <div class="card-icon bg-primary">
-                            <i class="far fa-user"></i>
+                        <div class="card-icon bg-success">
+                            <i class="fas fa-database"></i>
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
@@ -25,8 +25,8 @@
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
-                        <div class="card-icon bg-danger">
-                            <i class="far fa-newspaper"></i>
+                        <div class="card-icon bg-warning">
+                            <i class="fas fa-pencil-ruler"></i>
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
